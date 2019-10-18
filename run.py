5f5033f156edd782c7d5b928c094079cd3d3dc94
Yuki-Shibanuma <smatosan@gmail.com>
2019-10-19 07:59:22 +0900

    use

---
+++ run.py
@@ -0,0 +1,11 @@
+from classificate import classificate
+
+if __name__ == '__main__':
+    data = classificate("images/flower_photos/daisy/21652746_cc379e0eea_m.jpg")
+    print(data)
+    data = classificate("images/flower_photos/dandelion/7355522_b66e5d3078_m.jpg")
+    print(data)
+    data = classificate("images/flower_photos/dandelion/8181477_8cb77d2e0f_n.jpg")
+    print(data)
+    data = classificate("images/flower_photos/dandelion/13920113_f03e867ea7_m.jpg")
+    print(data)
